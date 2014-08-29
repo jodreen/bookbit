3a9e1f6335842080db9469099d11ac2846ac92f0
Jordeen Chang <jordeenchang@gmail.com>
2014-08-29 00:46:15 -0700

    Colors display when changing hex/validation works

--- app/models/hex.rb
+++ app/models/hex.rb
@@ -1,5 +1,6 @@
 class Hex < ActiveRecord::Base
 	belongs_to :user
 
-	validates :hexcode, format: { with: /\A[a-fA-F0-9]{6}\z/, on: :create, message: "not a valid hex code." }
+	validates :hexcode, format: { with: /\A[a-fA-F0-9]{6}\z/, message: "not a valid hex code." }
+
 end
